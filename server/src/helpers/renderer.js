d10cd6dda653a97d4d9e8de03053596f0af15186
AnastasiiaHusarenko <ahu@jooble.com>
2023-11-20 17:48:47 +0200

    project is done

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -5,12 +5,13 @@ import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import serialize from 'serialize-javascript';
 import { Helmet } from 'react-helmet';
+import Routes from '../client/Routes';
 
 export default (req, store, context) => {
     const content = renderToString(
         <Provider store={store}>
             <StaticRouter location={req.path} context={context}>
-                <div>{renderRoutes('Routes')}</div>
+                <div>{renderRoutes(Routes)}</div>
             </StaticRouter>
         </Provider>
     );
